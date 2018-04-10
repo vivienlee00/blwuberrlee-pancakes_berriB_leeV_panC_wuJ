2da72c07abc486e2cefa6ff2e2c58a18ce79c4a7
Bayan Berri <bberri@stuy.edu>
2018-04-10 13:17:35 -0400

    js starteD

---
+++ static/script.js
@@ -0,0 +1,15 @@
+/*
+BwuBerrLee-Pancakes
+SoftDev2 pd7
+*/
+//======================================================================
+//                          VAR DECLARATION
+//======================================================================
+
+var svg=document.getElementById("svg");
+var width=svg.getAttribute("width");
+var height=svg.getAttribute("height");
+
+var setup=function(){
+    
+}
