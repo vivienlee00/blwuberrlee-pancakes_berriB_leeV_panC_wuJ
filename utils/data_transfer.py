b252d6ee365bf4a56d6e0d58935157278091c64d
Carol217 <cpan3@stuy.edu>
2018-04-11 13:19:55 -0400

    trying

--- utils/data_transfer.py
+++ utils/data_transfer.py
@@ -10,14 +10,17 @@ global db
 global c
 
 def connect():
+    global db
+    global c
     f = "../data/dataset.db"
-    db = sqlite3.connect(f. check_same_thread = False)
-    c = db.cursor
-    stmt = "CREATE TABLE IF NOT EXISTS shootings (id INTEGER, name TEXT, location TEXT, date TEXT, descr TEXT, fatals INTEGER, injured INTEGER, total INTEGER, mental INTEGER, race TEXT, gender TEXT, lat TEXT, long TEXT)"
+    db = sqlite3.connect(f, check_same_thread = False)
+    c = db.cursor()
+    stmt = "CREATE TABLE IF NOT EXISTS shootings (id INTEGER PRIMARY KEY, name TEXT, location TEXT, date TEXT, descr TEXT, fatals INTEGER, injured INTEGER, total INTEGER, mental INTEGER, race TEXT, gender TEXT, lat TEXT, long TEXT)"
     c.execute(stmt)
     return
 
 def terminate():
+    global db
     db.commit()
     db.close()
     return
@@ -26,12 +29,30 @@ global csvfile
 global reader
 
 def openfile():
+    global csvfile
+    global reader
     csvfile = open('../data/shootings.csv','r')
     reader = csv.DictReader(csvfile)
     return
 
 def closefile():
+    global csvfile
     csvfile.close();
     return
 
-global command = "INSERT INTO shootings VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)"
+
+global command
+command = "INSERT INTO shootings VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)"
+
+def transfer():
+    global reader
+    global c
+    connect()
+    openfile()
+    for r in reader:
+        c.execute(command, (r["S#"], r["Title"], r["Location"], r["Date"], r["Summary"], r["Fatalities"], r["Injured"], r["Total victims"], r["Mental Health Issues"], r["Race"], r["Gender"], r["Latitude"], r["Longitude"]))
+    closefile()
+    terminate()
+    return
+
+transfer()
