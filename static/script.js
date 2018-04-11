4a6d81e39f058dfba21dedf577540527ad9df8aa
vivienlee00 <vivien.lee.601@gmail.com>
2018-04-11 19:56:26 -0400

    added timeline axisgit add *!

--- static/script.js
+++ static/script.js
@@ -6,10 +6,25 @@ SoftDev2 pd7
 //                          VAR DECLARATION
 //======================================================================
 
-var svg=document.getElementById("svg");
-var width=svg.getAttribute("width");
-var height=svg.getAttribute("height");
+var svg=d3.select("svg");
+var width= svg.attr("width");
+var height= svg.attr("height");
+console.log(height);
+
+var margin = { top: 40, right: 40, bottom: 100, left: 40 };
+
+//var data = [10, 15, 20, 25, 30]; //change to fit dataset later on
 
 var setup=function(){
-    
+
 }
+
+var xScale = d3.scaleLinear()
+.domain([1960, 2020])
+.range([margin.left, width - margin.right]);
+
+var xAxis = d3.axisBottom(xScale);
+
+var bottomaxis = svg.append("g")
+bottomaxis.call(xAxis);
+bottomaxis.attr("transform", "translate(0," + (height-margin.bottom) + ")");
