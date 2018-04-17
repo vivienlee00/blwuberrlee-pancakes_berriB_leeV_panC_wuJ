984b480a51409469be85dadf7da5a1a4ac352b04
Joyce Wu <jwu26@stuy.edu>
2018-04-16 22:53:17 -0400

    converted csv file to json file

--- utils/parse.py
+++ utils/parse.py
@@ -3,34 +3,19 @@
 
 import csv
 import json
-import codecs
 
 #convert shootings csv file to json file
 def convert():
-    d = "{"
-    f = open('../data/shootings.csv', 'r')
-    s = f.read()
-    data = s.split(',')
-    #S#,Title,Location,Date,Summary,Fatalities,Injured,Total victims,Mental Health Issues,Race,Gender,Latitude,Longitude
-    for(i = 0; i < 12; i++):
-        for(row = 1; row < data.length/13, row++):
-            if i == 0:
-            else if i == 1:
+    with open('../data/shootings.csv') as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+    with open('../data/shootings.json', 'w') as f:
+        json.dump(rows, f)
 
-    # data = []
-    # with codecs.open('../data/shootings.csv', 'r', encoding='utf-8', errors='ignore') as f:
-    #     for row in csv.DictReader(f):
-    #         data.append(row)
-    # json_data = json.dumps(data)
-    # print json_data
+# convert()
 
-    # f = open('../data/shootings.csv', 'r')
-    # s = f.read()
-    # str = unicode(s, errors='ignore')
-    # #print(str)
-    # reader = csv.DictReader(str)
-    # rows = list(reader)
-    # with open('../data/shootings.json', 'w') as f:
-    #     json.dumps(rows, f)
+#loads json file
+with open('../data/shootings.json') as f:
+    data = json.load(f)
 
-convert()
+print(data[0]['Title'])
